5294feabb0ff2db2f94d216890e5600e73429463
Wannes Pipien <wannes.pipien@student.pxl.be>
2024-10-05 16:29:25 +0200

    Opdracht toegevoegd

---
+++ Snelste atleet/Snelste atleet/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Snelste_atleet
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Snelste atleet/Snelste atleet/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Snelste_atleet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string name;
+        int time = 0;
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+            int currentTime = int.Parse(timeTextBox.Text);
+
+            if (currentTime < time || time == 0)
+            {
+                name = nameTextBox.Text;
+                time = currentTime;
+                nameTextBox.Clear();
+                timeTextBox.Clear();   
+                nameTextBox.Focus();
+            }
+            else
+            {
+                nameTextBox.Clear();
+                timeTextBox.Clear();
+                nameTextBox.Focus();
+            }
+        }
+
+        private void fastButton_Click(object sender, RoutedEventArgs e)
+        {
+            TimeSpan t = TimeSpan.FromSeconds(time);
+            string answer = string.Format("{0:D2}uur\n{1:D2}minuten\n:{2:D2}seconden",
+            t.Hours,
+            t.Minutes,
+            t.Seconds);
+
+            resultTextBox.Text = $"De snelste atleet is {name}.\nTotale tijd {time} seconden.\n\n{answer}";
+
+        }
+
+        private void erasebutton_Click(object sender, RoutedEventArgs e)
+        {
+            nameTextBox.Clear();
+            timeTextBox.Clear();
+            resultTextBox.Clear();
+        }
+
+        private void closebutton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
